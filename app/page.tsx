9363712ee626feae65d2d80ce2435a0412356693
Samuel Ademola <samuel.ademola43@gmail.com>
2024-02-15 23:01:40 +0000

    remove uncessary import on page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Image from "next/image";
+// import Image from "next/image";
 // import styles from "./page.module.css";
 import SubTeam from "./components/Sub-Teams/Sub-Team";
 
